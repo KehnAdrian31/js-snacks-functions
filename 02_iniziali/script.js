57a510492b2303b9f1337a064f0711ce30099e1b
KehnAdrian31 <kehn000panganibangmail.com>
2025-07-10 08:15:39 +0200

    [fix] 2° esercizio

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function nameInitials(arrayInitials){
+  const initials =[];
+  for(let i = 0; i < arrayInitials.length; i++){
+    initials.push(arrayInitials[i][0]);
+  }
+
+  return initials;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(nameInitials(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
