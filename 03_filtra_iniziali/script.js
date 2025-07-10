537fd471b24690c98f3bbd29b7e7e3ff033173d5
KehnAdrian31 <kehn000panganibangmail.com>
2025-07-10 08:28:13 +0200

    [fix] 3° esercizio

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,22 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function findInitials(array, letter){
+  //definisco la variabile array
+  const filter = []
 
+  //ciclo l' array
+  for(let i=0; i<array.length; i++){
+    if(array[i][0].toLowerCase() === letter.toLowerCase()){
+      filter.push(array[i]);
+    }
+  }
+
+  return filter;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(findInitials(names, 'a'))
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
