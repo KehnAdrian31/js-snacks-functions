0572e6caa2b4e8ae011d2f1b113495286f1d9c9a
KehnAdrian31 <kehn000panganibangmail.com>
2025-07-10 09:26:30 +0200

    4° esercizio

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,22 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function countVowels(string){
+  //dichiaro un array di vocali
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  //dichiaro una variabile che conta le vocali all ' interno della parola
+  let count = 0;
+//ciclo la parila
+for(let i=0; i<string.length; i++){
+  if(vowels.includes(string[i])){
+    count++;
+  }
+}
 
-
+return count
+}
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(countVowels(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
